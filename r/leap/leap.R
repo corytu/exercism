ac43f891dabda6f76bfcf4b11283f28a521cd4a3
corytu <corytu@users.noreply.github.com>
2019-11-25 19:33:30 +0800

    Refactor the solution - return T/F directly

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -1,7 +1,3 @@
 leap <- function(year) {
-  if (year %% 400 == 0 | (year %% 4 == 0 & year %% 100 != 0)) {
-    return(TRUE)
-  } else {
-    return(FALSE)
-  }
+  return(year %% 400 == 0 | (year %% 4 == 0 & year %% 100 != 0))
 }
