a7cc2195499a08efb4b233a0e3fd2c436ca2d79c
corytu <corytu@users.noreply.github.com>
2019-12-11 23:32:25 +0800

    Use ifelse function instead of explicit if-else

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,9 +1,5 @@
 raindrops <- function(number) {
   sounds <- list("Pling", "Plang", "Plong")
   mask <- c(number %% 3 == 0, number %% 5 == 0, number %% 7 == 0)
-  if (any(mask)) {
-    do.call(paste0, sounds[mask])
-  } else {
-    as.character(number)
-  }
+  ifelse(any(mask), do.call(paste0, sounds[mask]), as.character(number))
 }
