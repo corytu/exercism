0b448bb70e9af8cc0c8a4fd3d0e639283e40710d
corytu <corytu@users.noreply.github.com>
2019-12-19 23:17:49 +0800

    Use nchar to check lengths of characters

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,9 +1,9 @@
 # This is a stub function to take two strings
 # and calculate the hamming distance
 hamming <- function(strand1, strand2) {
-  nucleotides_list <- strsplit(c(strand1, strand2), "")
-  if (length(unique(sapply(nucleotides_list, length))) > 1) {
+  if (nchar(strand1) != nchar(strand2)) {
     stop("Strand 1 and strand 2 have unequal lengths")
   }
+  nucleotides_list <- strsplit(c(strand1, strand2), "")
   sum(nucleotides_list[[1]] != nucleotides_list[[2]])
 }
