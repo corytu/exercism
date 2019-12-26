0fdc3c1158ffc693e351def3ae32592a181c1929
corytu <corytu@users.noreply.github.com>
2019-12-27 00:23:11 +0800

    Iterate strings without converting them into lists

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,6 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strand 1 and strand 2 have unequal lengths")
-    strand_a_list = list(strand_a)
-    strand_b_list = list(strand_b)
-    return sum([i != j for i, j in zip(strand_a_list, strand_b_list)])
+    return sum([i != j for i, j in zip(strand_a, strand_b)])
